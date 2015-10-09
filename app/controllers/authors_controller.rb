904ccea9c1b2bc48d12169555d0bb8d4de8312bb
Vivek <vivek@avanoo.com>
2015-10-09 12:42:41 -0700

    Implement authors controller actions

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,6 @@
 class AuthorsController < ApplicationController
+  before_action :set_author, except: [:index, :new, :create]
+
   def index
     @authors = Author.all
   end
@@ -11,4 +13,49 @@ class AuthorsController < ApplicationController
   def new
     @author = Author.new
   end
+
+  def create
+    # require 'pry'; binding.pry
+    @author = Author.new(author_params)
+    if @author.save
+      flash[:success] = 'Author has been created.'
+      redirect_to @author
+    else
+      flash[:danger] = 'Author has not been created.'
+      render :new
+    end
+  end
+
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    @author = Author.find(params[:id])
+    if @author.update(author_params)
+      flash[:success] = "Author has been updated."
+      redirect_to @author
+    else
+      flash[:danger] = "Author has not been updated."
+      render :edit
+    end
+  end
+
+  def destroy
+    @author = Author.find(params[:id])
+    if @author.destroy
+      flash[:success] = "Author has been deleted."
+      redirect_to authors_path
+    end
+  end
+
+private
+  def author_params
+    params.require(:author).permit(:first_name, :last_name)
+  end
+
+  def set_author
+    @author = Author.find(params[:id])
+  end
+
 end
