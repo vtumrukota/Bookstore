fb971331c0a9f4bf0c5c2747a14f34be85f343d2
Vivek <vivek@avanoo.com>
2015-10-09 16:31:44 -0700

    Add Book Model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151009224729) do
+ActiveRecord::Schema.define(version: 20151009231734) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "first_name"
@@ -19,6 +19,20 @@ ActiveRecord::Schema.define(version: 20151009224729) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.string   "isbn",            limit: 13
+    t.text     "description"
+    t.date     "published_at"
+    t.integer  "publisher_id_id"
+    t.integer  "page_count"
+    t.decimal  "price",                      precision: 16, scale: 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "books", ["publisher_id_id"], name: "index_books_on_publisher_id_id"
 
   create_table "publications", force: :cascade do |t|
     t.integer  "author_id"
